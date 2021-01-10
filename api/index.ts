ddff2f4068f525df190907c7bfcf1479f0fb6612
Serj1c <sergej.a.isaev@gmail.com>
2021-01-11 00:00:59 +0300

    folders renamed + cleaning

--- api/index.ts
+++ api/index.ts
@@ -1,13 +1,20 @@
-import express, { Application, Request, Response } from 'express';
-
-const app: Application = express();
+import express from 'express';
+import mongoose from 'mongoose';
 
 const PORT = process.env.PORT || 3001;
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('dead timer!')
-})
+const app = express();
+
+app.use(express.json());
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-});
+    console.log(`Server is listening on port ${PORT}`);
+})
+
+// database config
+const db = require('./config/default').mongoURI;
+
+// connect to database
+mongoose.connect(db, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true })
+    .then(() => console.log("Mongo is here!"))
+    .catch((err: any) => console.log(err));
